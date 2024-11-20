d80d99aec75323f8c4c7ea99009fee14c9b547c1
sharathevega <sharathkumarevega@gmail.com>
2024-11-20 17:45:15 +0530

    rrr

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,14 @@
 'use client';
-import { useRouter } from 'next/navigation';
+import Form from './Components/Form';
 
 const HomePage = () => {
-  const router = useRouter();
-
-  const handleStartRegistration = () => {
-    router.push('/register'); // Redirect to the registration form
-  };
-
   return (
     <div className="container mt-5 text-center">
       <h1>Welcome to the Payment Integration App</h1>
       <p className="mt-3">
         This app demonstrates a complete flow of user registration followed by a secure payment gateway integration.
       </p>
-     
+      <Form />
     </div>
   );
 };
